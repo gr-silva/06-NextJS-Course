ec00231e813b16395522307f51e7e60b2536d55d
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-03-11 22:05:43 -0300

    fix: trim command space

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,7 +1,7 @@
 const { execSync } = require("child_process");
 
 console.log("[DEPLOY_PREVIEW]: START");
-const command = "yarn deploy:staging ";
+const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: "utf-8" });
 console.log(output);
 console.log("[DEPLOY_PREVIEW]: END");
